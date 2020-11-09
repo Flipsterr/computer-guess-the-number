ccdc96382fe5faf6c2e6785c708264f0e44eff18
unknown <filip.von.bromssen@gmail.comm>
2020-11-09 15:58:28 +0100

    binär gissning wip

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let guess
+let guess = 
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
-
+    guess/2
+    setMessage(`Is it ${guess}?`)
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
-    
+    (guess*2)*0.75
+    setMessage(`Is it ${guess}?`)
+})
+
+document.getElementById("is-correct-btn").addEventListener("click", function(){
+    setMessage("woohooo!")
 })
 
 
@@ -18,13 +22,13 @@ function setMessage(msg) {
     document.getElementById('message').innerText = msg
 }
 
-function start() {
+function start() { 
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    guess = 50       /*Math.floor(Math.random()*101)*/
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
 
 document
